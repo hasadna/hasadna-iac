a7d2199a4ef01ae8f7a141dba1b0c387f116df5b
Ori Hoch <ori@uumpa.com>
2022-06-16 14:18:59 +0300

    add hasadna nfs1 internal ip output

---
+++ modules/hasadna/outputs.tf
@@ -0,0 +1,3 @@
+output "hasadna_nfs1_internal_ip" {
+  value = kamatera_server.hasadna_nfs1.private_ips[0]
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "hasadna_nfs1_internal_ip" {
+  value = module.hasadna.hasadna_nfs1_internal_ip
+}
