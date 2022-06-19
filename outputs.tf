64dbf52c068a178bfd4f23954a7d56a60b66b212
Ori Hoch <ori@uumpa.com>
2022-06-19 16:46:01 +0300

    add ssh access point port to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "hasadna_ssh_access_point_public_ip" {
   value = module.hasadna.hasadna_ssh_access_point_public_ip
   sensitive = true
 }
+
+output "hasadna_ssh_access_point_public_port" {
+  value = var.hasadna_ssh_access_point_ssh_port
+  sensitive = true
+}
