9abc4ee682d02107f2b433798507cc8351425605
Ori Hoch <ori@uumpa.com>
2023-04-24 18:09:47 +0300

    remove israeltiles s3 bucket

--- modules/hasadna/aws_s3_buckets.tf
+++ modules/hasadna/aws_s3_buckets.tf
@@ -13,13 +13,6 @@ resource "aws_s3_bucket" "hasadna_discourse_backup" {
     }
 }
 
-resource "aws_s3_bucket" "israeltiles" {
-    bucket = "israeltiles"
-    # We are not sure what this bucket is for, we blocked public access and waiting to see if anyone complains (Dec 27, 2022)
-    # The bucket is already archived in glacier, so if no one complains we can delete it
-}
-
-
 resource "aws_s3_bucket" "hasadna_archive_cold_storage" {
     bucket = "hasadna-archive-cold-storage"
     provider = aws.eu_west_3
