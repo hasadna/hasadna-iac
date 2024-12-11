b7ea6d736897e03e4b1138d2d9601ca47d6306df
Ori Hoch <ori@uumpa.com>
2024-12-11 15:08:00 +0200

    add hasadna k8s github app

---
+++ modules/hasadna/hasadna_k8s_github_app.tf
@@ -0,0 +1,6 @@
+# App was created manually in the GitHub UI
+# Private key was generated and uploaded to Vault at `Projects/k8s/github-app` under key `private-key.pem`
+# App was manually installed on `hasadna` organization for all current and future repositories
+# following values are also stored in Vault at `Projects/k8s/github-app`:
+# - `app_id`
+# - `installation_id`
