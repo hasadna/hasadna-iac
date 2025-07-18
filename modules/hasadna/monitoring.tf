d6ef0c1914944655680f32442b52baa01cf8364b
OriHoch <ori@uumpa.com>
2025-07-18 22:03:45 +0300

    add slack alerts

---
+++ modules/hasadna/monitoring.tf
@@ -0,0 +1,5 @@
+# slack https://api.slack.com/apps -> create new app "hasadna-iac-monitoring" in Public Knowledge Slack workspace
+#    enable incoming webhooks
+#    add webhook to channel cluster-rke2-notifications
+#    copy the webhook URL
+#    paste in Vault `Projects/iac/monitoring` under `slack-webhook-cluster-rke2-notifications`
