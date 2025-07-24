dfefc56d43dcdab37fd00a6f1ee6316dad01fe17
Ori Hoch <ori@uumpa.com>
2025-07-24 12:48:25 +0300

    improve stride backup

--- modules/openbus/stride_db_backup.sh
+++ modules/openbus/stride_db_backup.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 
 cd /var/lib/postgresql
+if [ -f ./stride_db.pid ]; then
+  echo found existing stride_db.pid file, will not run
+  exit 0
+fi
+echo $$ > ./stride_db.pid
 echo `date +"%Y-%m-%d %H:%M"` creating stride_db backup
 pg_dump -n public --no-privileges | zstd -19 -o ./stride_db.sql.zst
 du -h ./stride_db.sql.zst
@@ -13,4 +18,6 @@ rm ./stride_db.sql.zst
 echo `date +"%Y-%m-%d %H:%M"` sending heartbeat to StatusCake
 curl "$STRIDE_DB_BACKUP_CHECK_URL"
 echo `date +"%Y-%m-%d %H:%M"` heartbeat sent to StatusCake
+rm ./stride_db.pid
 echo `date +"%Y-%m-%d %H:%M"` Great Success!
+exit 0
