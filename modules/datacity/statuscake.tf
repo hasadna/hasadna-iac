e4ac50e223628c31a33267e12e3b8862aef38e88
OriHoch <ori@uumpa.com>
2025-07-29 11:10:34 +0300

    increase datacity alert thresholds

--- modules/datacity/statuscake.tf
+++ modules/datacity/statuscake.tf
@@ -2,13 +2,15 @@ resource "statuscake_uptime_check" "apps" {
     for_each = {for site in local.sites: site.name => site if !site.disabled}
     name = "datacity-${each.key}"
     tags = ["hasadna-iac", "datacity", "datacity-${each.key}"]
-    check_interval = 60
+    check_interval = 60 * 30  # every 30 minutes
     confirmation = 3
+    trigger_rate = 5
     http_check {
         status_codes = [204, 205, 206, 303, 400, 401, 403, 404, 405, 406, 408, 410, 413, 444, 429, 494, 495, 496, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 521, 522, 523, 524, 520, 598, 599]
         validate_ssl = true
         follow_redirects = true
-        timeout = 15
+        timeout = 40
+        user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 (hasadna-iac datacity statuscake)"
     }
     monitored_resource {
         address = each.value.url
@@ -20,20 +22,17 @@ resource "statuscake_uptime_check" "datasets_count" {
     for_each = {for site in local.sites: site.name => site if !site.disabled}
     name = "datacity-${each.key}-datasets-count"
     tags = ["hasadna-iac", "datacity", "datacity-${each.key}", "datasets-count"]
-    check_interval = 60
+    check_interval = 60 * 60  # every 1 hour
     confirmation = 3
+    trigger_rate = 5
     http_check {
         status_codes = [204, 205, 206, 303, 400, 401, 403, 404, 405, 406, 408, 410, 413, 444, 429, 494, 495, 496, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 521, 522, 523, 524, 520, 598, 599]
         validate_ssl = true
         follow_redirects = true
-        timeout = 15
-        content_matchers {
-            content = "\"count\": 0"
-            matcher = "NOT_CONTAINS_STRING"
-        }
+        timeout = 40
     }
     monitored_resource {
-        address = "${each.value.url}/api/3/action/package_search?rows=1"
+        address = "${each.value.url}/dataset"
     }
     contact_groups = [234311]
 }
