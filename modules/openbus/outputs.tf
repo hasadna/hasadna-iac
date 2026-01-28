02f3415a60f90770a30ad3058d47d42f8d36c79c
Ori Hoch <ori@uumpa.com>
2026-01-28 15:03:31 +0200

    openbus update

--- modules/openbus/outputs.tf
+++ modules/openbus/outputs.tf
@@ -1,6 +1,17 @@
+data "vault_kv_secret_v2" "openbus_ingress_nginx_configuration_snippet" {
+  mount = "kv"
+  name = "Projects/OBus/nginx-ingress-configuration-snipper"
+}
+
 output "kubernetes_tf_outputs" {
-  value = {
-    for name in concat(local.ingress_names, ["open-bus-stride-api"]):
-      "ingress-${name}" => "${name}.${var.cloudflare_zone_hasadna_org_il.name}"
-  }
+  value = merge(
+    {
+      openbus-ingress-nginx-configuration-snippet = data.vault_kv_secret_v2.openbus_ingress_nginx_configuration_snippet.data["snippet"],
+      openbus-ingress-nginx-denylist-source-range = data.vault_kv_secret_v2.openbus_ingress_nginx_configuration_snippet.data["denylist"],
+    },
+    {
+      for name in concat(local.ingress_names, ["open-bus-stride-api"]):
+        "ingress-${name}" => "${name}.${var.cloudflare_zone_hasadna_org_il.name}"
+    }
+  )
 }
